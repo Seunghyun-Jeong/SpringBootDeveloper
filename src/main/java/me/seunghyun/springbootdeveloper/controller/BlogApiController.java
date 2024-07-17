77ba07909b46e251360ec52e69748bac6f49c148
Seunghyun-Jeong <dkskakdgka@gmail.com>
2024-07-17 21:01:52 +0900

    feat: 블로그 글 목록 조회 api 구현

--- src/main/java/me/seunghyun/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/seunghyun/springbootdeveloper/controller/BlogApiController.java
@@ -1,11 +1,14 @@
 package me.seunghyun.springbootdeveloper.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import me.seunghyun.springbootdeveloper.domain.Article;
 import me.seunghyun.springbootdeveloper.dto.AddArticleRequest;
+import me.seunghyun.springbootdeveloper.dto.ArticleResponse;
 import me.seunghyun.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +25,12 @@ public class BlogApiController {
         Article savedArticle = blogService.save(request);
         // 요청한 자원이 성공적으로 생성되었으며 저장된 블로그 글 정보를 응답 객체에 담아 죄송
         return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll().stream().map(ArticleResponse::new).toList();
+
+        return ResponseEntity.ok().body(articles);
     }
 }
