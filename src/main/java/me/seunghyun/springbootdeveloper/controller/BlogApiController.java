344dea1e98ad9d33885540f795f8588cd6a2b0df
Seunghyun-Jeong <dkskakdgka@gmail.com>
2024-07-17 23:03:52 +0900

    feat: 블로그 글 단건 조회 기능 추가

--- src/main/java/me/seunghyun/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/seunghyun/springbootdeveloper/controller/BlogApiController.java
@@ -9,6 +9,7 @@ import me.seunghyun.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +33,13 @@ public class BlogApiController {
         List<ArticleResponse> articles = blogService.findAll().stream().map(ArticleResponse::new).toList();
 
         return ResponseEntity.ok().body(articles);
+    }
+
+    @GetMapping("/api/articles/{id}")
+    // URL 경로에서 값 추출
+    public ResponseEntity<ArticleResponse> findArticles(@PathVariable long id) {
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok().body(new ArticleResponse(article));
     }
 }
